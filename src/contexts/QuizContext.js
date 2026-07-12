f7a6b9ff2cd66168520c38311b5bd67140c6f1fa
Hamzv8 <hamzaatef722@gmail.com>
2026-07-12 17:13:33 +0300

    Update QuizContext.js and questions file

--- src/contexts/QuizContext.js
+++ src/contexts/QuizContext.js
@@ -82,12 +82,17 @@ function QuizProvider({ children }) {
     0
   );
 
-  useEffect(function () {
-    fetch("http://localhost:9000/questions")
-      .then((res) => res.json())
-      .then((data) => dispatch({ type: "dataReceived", payload: data }))
-      .catch((err) => dispatch({ type: "dataFailed" }));
-  }, []);
+useEffect(function () {
+  fetch("/questions.json")
+    .then((res) => res.json())
+    .then((data) =>
+      dispatch({
+        type: "dataReceived",
+        payload: data.questions,
+      })
+    )
+    .catch(() => dispatch({ type: "dataFailed" }));
+}, []);
 
   return (
     <QuizContext.Provider
